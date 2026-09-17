ad7b8fb34f1e8da4e868733bdf8e111ca26c123f
Nasora-group <ndbpharm365@gmail.com>
2026-09-17 11:26:47 +0000

    test: cover health endpoint and sitemap hygiene

--- tests/test_public_seo.py
+++ tests/test_public_seo.py
@@ -8,18 +8,25 @@ def test_public_seo_routes_are_available():
 
     robots = client.get("/robots.txt")
     sitemap = client.get("/sitemap.xml")
+    health = client.get("/healthz")
 
     assert robots.status_code == 200
     assert robots.mimetype == "text/plain"
-    assert "Sitemap:" in robots.get_data(as_text=True)
-    assert "Disallow: /api/" in robots.get_data(as_text=True)
+    robots_text = robots.get_data(as_text=True)
+    assert "Sitemap:" in robots_text
+    assert "Disallow: /api/" in robots_text
+    assert "Disallow: /login" in robots_text
 
     assert sitemap.status_code == 200
     assert sitemap.mimetype == "application/xml"
     xml = sitemap.get_data(as_text=True)
-    assert "https://" not in xml or "<urlset" in xml
+    assert "<urlset" in xml
     assert "/rgpd" in xml
     assert "/cgu" in xml
+    assert "/login" not in xml
+
+    assert health.status_code == 200
+    assert health.get_data(as_text=True) == "ok\n"
 
 
 def test_api_responses_are_marked_non_indexable():
