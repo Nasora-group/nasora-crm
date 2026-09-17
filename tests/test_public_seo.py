d3fdf8eba8dc0f16872dfa2672d96767af8c3c59
Nasora-group <ndbpharm365@gmail.com>
2026-09-17 17:35:52 +0000

    Test public SEO caching and safety

--- tests/test_public_seo.py
+++ tests/test_public_seo.py
@@ -12,6 +12,7 @@ def test_public_seo_routes_are_available():
 
     assert robots.status_code == 200
     assert robots.mimetype == "text/plain"
+    assert robots.headers["Cache-Control"] == "public, max-age=3600"
     robots_text = robots.get_data(as_text=True)
     assert "Sitemap:" in robots_text
     assert "Disallow: /api/" in robots_text
@@ -19,11 +20,13 @@ def test_public_seo_routes_are_available():
 
     assert sitemap.status_code == 200
     assert sitemap.mimetype == "application/xml"
+    assert sitemap.headers["Cache-Control"] == "public, max-age=3600"
     xml = sitemap.get_data(as_text=True)
     assert "<urlset" in xml
     assert "/rgpd" in xml
     assert "/cgu" in xml
     assert "/login" not in xml
+    assert "&amp;" not in xml
 
     assert health.status_code == 200
     assert health.get_data(as_text=True) == "ok\n"
